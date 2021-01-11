7c46167d848d865b5c5fc4f5da9190de418d70df
Feruzjon Muyassarov <feruzjon.muyassarov@est.tech>
2021-01-12 00:14:45 +0200

    add docker installation

--- hack/install_docker.sh
+++ hack/install_docker.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install \
+apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
 
 docker ps
 
